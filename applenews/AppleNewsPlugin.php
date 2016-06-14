43c223ee9e24485c6834d61e6e26996c10faf39c
Brandon Kelly <brandon@pixelandtonic.com>
2016-06-14 09:06:39 -0700

    1.0.0

--- applenews/AppleNewsPlugin.php
+++ applenews/AppleNewsPlugin.php
@@ -32,7 +32,7 @@ class AppleNewsPlugin extends BasePlugin
      */
     public function getVersion()
     {
-        return '1.0';
+        return '1.0.0';
     }
 
     public function getSchemaVersion()
